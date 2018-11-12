eba63fa879f527018a010fa4bdad7fc36f66fc89
김병곤 <qudrhs190@gmail.com>
2018-11-12 14:36:31 +0900

    Lower case

--- CONVERTER.cpp
+++ CONVERTER.cpp
@@ -5,14 +5,16 @@
 #include "IndoorGML_Writer/IndoorGML_Writer.h"
 #include "OSM_Reader/OSM_Reader.h"
 #include "OSM_Writer/OSM_Writer.h"
+#include "IO/IO.h"
 #include <iostream>
 #include <string.h>
 
 int main(int argc, char **argv) {
-    if(strcmp(strlwr(argv[1]),"indoorgml")==0 && strcmp(strlwr(argv[2]),"osm")==0){
+
+    if(strcmp(IO::lowercase(argv[1]),"indoorgml")==0 && strcmp(IO::lowercase(argv[2]),"osm")==0){
         OSM::Write(INDOOR::Read(argv[3]),argv[4]);
     }
-    if(strcmp(strlwr(argv[1]),"osm")==0 && strcmp(strlwr(argv[2]),"indoorgml")==0){
+    if(strcmp(IO::lowercase(argv[1]),"osm")==0 && strcmp(IO::lowercase(argv[2]),"indoorgml")==0){
         INDOOR::Write(OSM::Read(argv[3]),argv[4]);
     }
     return 0;
