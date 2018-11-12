437a040294b56294304c33b800bb894a8d3be5c7
김병곤 <qudrhs190@gmail.com>
2018-11-12 14:35:55 +0900

    Control Input

---
+++ IO/IO.cpp
@@ -0,0 +1,19 @@
+//
+// Created by byeonggon on 2018-11-12.
+//
+
+#include "IO.h"
+#include <stdio.h>
+#include <ctype.h>
+namespace IO{
+    char * lowercase(char *input){
+        int i=0;
+        char c;
+        while(input[i]){
+            c=input[i];
+            putchar(tolower(c));
+            i++;
+        }
+        return input;
+    }
+}
