162fcae5d40eba08ff86fca39ac923414869062d
김병곤 <qudrhs190@gmail.com>
2018-11-14 14:38:52 +0900

    REVISE

--- DOC/mainpage.h
+++ DOC/mainpage.h
@@ -4,13 +4,13 @@
  *
  * @section intro_sec Introduction
  *
- * IndoorGML <-> OSM converter
+ * IndoorGML <-> Openstreetmap converter
  *
  *
  * @section dependencies_sec Dependencies
  *
 - InFactory (https://github.com/STEMLab/InFactory) : To generate IndoorGML
-- OSM (https://wiki.openstreetmap.org/wiki/Main_Page) : OSM wiki
+- OSM (https://wiki.openstreetmap.org/wiki/Main_Page) : Openstreetmap wiki
 - rapidxml (http://rapidxml.sourceforge.net) : To read COLLADA(*.dae) file
  *
  * @section geometric_features_sec Geometric Indoor Features
@@ -22,16 +22,15 @@
  * - CONVERTER::IC
  *
  *
- * - TM2IN::MinimumBoundingBox
  *
  * Basic geometric features are TM2IN::Vertex and TM2IN::HalfEdge.
  *
- *  - Indoor_OSM::CONVERTER::IC
- *  - Indoor_OSM::CONVERTER::Pos
- *  - Indoor_OSM::CONVERTER::CellSpace
- *  - Indoor_OSM::CONVERTER::CellSpaceBoundary
- *  - Indoor_OSM::CONVERTER::State
- *  - Indoor_OSM::CONVERTER::Transition
+ *  - CONVERTER::IC
+ *  - CONVERTER::Pos
+ *  - CONVERTER::CellSpace
+ *  - CONVERTER::CellSpaceBoundary
+ *  - CONVERTER::State
+ *  - CONVERTER::Transition
  * @section released Released
  *
  * Now released version of CONVERTER.cpp 1.0.0
